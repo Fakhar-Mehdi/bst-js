a386618f11d1ab13be7de0412f41c440c425e40e
bilalashraf1710 <fakhar.mehdi@ocloudsolutions.net>
2023-12-07 16:29:37 +0500

    feedback inculcated. made class Node  and BST

--- Node.js
+++ Node.js
@@ -1,53 +1,58 @@
-export default class Node {
+class Node {
     constructor(value) {
         this.value = value
         this.left = null
         this.right = null
     }
+}
+
+export default class BST {
+    constructor(value) { this.root = new Node(value) }
 
     addNode(value) {
         value = parseInt(value)
         if (isNaN(value)) return
-        if (value < this.value) this.left ? this.left.addNode(value) : this.left = new Node(value)
-        else if (value > this.value) this.right ? this.right.addNode(value) : this.right = new Node(value)
+
+        if (value < this.root.value) this.root.left ? this.root.left.addNode(value) : this.root.left = new BST(value)
+        else if (value > this.root.value) this.root.right ? this.root.right.addNode(value) : this.root.right = new BST(value)
     }
 
     inOrder() {
-        if (this.left) this.left.inOrder()
-        console.log(this.value)
-        if (this.right) this.right.inOrder()
+        if (this.root?.left) this.root.left.inOrder()
+        console.log(this.root.value)
+        if (this.root?.right) this.root.right.inOrder()
     }
 
     preOrder() {
-        console.log(this.value)
-        if (this.left) this.left.preOrder()
-        if (this.right) this.right.preOrder()
+        console.log(this.root.value)
+        if (this.root.left) this.root.left.preOrder()
+        if (this.root.right) this.root.right.preOrder()
     }
 
     postOrder() {
-        if (this.left) this.left.postOrder()
-        if (this.right) this.right.postOrder()
-        console.log(this.value)
+        if (this.root.left) this.root.left.postOrder()
+        if (this.root.right) this.root.right.postOrder()
+        console.log(this.root.value)
     }
 
     _searchNode(parent, child, value) {
-        if (!child) return null
-
+        if (!child) return { parent: null, child: null }
         if (child.value === value) return { parent, child }
-        return value < child.value ? this._searchNode(child, child.left, value) : this._searchNode(child, child.right, value)
+        return value < child.value ? this._searchNode(child, child.left?.root, value) : this._searchNode(child, child.right?.root, value)
     }
 
-    _findInorderSuccessor() {
-        return this.left ? this.left._findInorderSuccessor() : this
+    _findInorderSuccessor() { 
+        return this.root.left ? this.root.left._findInorderSuccessor() : this.root
     }
 
     deleteNode(value) {
-        const { parent, child } = this._searchNode(null, this, value)
+        const { parent, child } = this._searchNode(null, this.root, value)
+        
         if (!child) return
 
         //Delete a Leaf Node
         if (!child.left && !child.right) {
-            parent.left.value === value ? parent.left = null : parent.right = null
+            parent.left?.root?.value === value ? parent.left = null : parent.right = null
             return
         }
 
